a90a6e07b88a4b63c5f1d4ee33a1d55de2ff3756
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-29 13:32:23 +0600

    insertion updated

--- NoteBook.js
+++ NoteBook.js
@@ -20,19 +20,29 @@ const displayNotes = () => {
 
 let notification = document.getElementById("notification");
 
-const addNote = () => {
-  let title = document.getElementById("title").value;
-  let note = document.getElementById("note").value;
+const addNote = async() => {
+  let title = await document.getElementById("title").value;
+  let note = await document.getElementById("note").value;
+  if (!title || !note) {
+    // Display an error message if either field is empty
+    let notificationContent = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <strong>Error!</strong> Please fill in both Title and Note fields before saving.
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>`;
+    notification.innerHTML = notificationContent;
+    return;
+  }
+
   localStorage.setItem(title, note);
 
   let notificationContent = "";
   notificationContent += `<div class="alert alert-warning alert-dismissible fade show" role="alert">
     <strong>Success!</strong> Your note has been saved successfully to NoteBook.
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-  </div>
-  `;
+  </div>`;
   notification.innerHTML = notificationContent;
   displayNotes();
+
 };
 
 const deleteNote = (key, card) => {
